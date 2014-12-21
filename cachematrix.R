3d4d3bcf0ffa8a1f6d0acfabb8a41c300a2d718c
Raynell Ortega <rortegapulido@gmail.com>
2014-12-20 23:35:31 -0500

    Script makeCacheMatrix v2.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Return the inverse of the matrix which is keep cached for save computation cost
 
 ## Write a short comment describing this function
+##This function contains sub-functions to operate with the Matrix that we want to inverse. 
+## set: Write the values of the matrix we are going to work with. We use here the reference to the parent environement.
+## get: Display the values of the matrix we have worked with.
+## getinverse: Get the values of the inverse matrix we have worked with. We use here the reference to the parent environement.
+## setinverse: Display the values of the inversed matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -10,8 +14,8 @@ makeCacheMatrix <- function(x = matrix()) {
   
   set <- function(y) {
     
-   x <<- y
-   inverseMatrix <<-  NULL
+    x <<- y
+    inverseMatrix <<-  NULL
     
   }
   
@@ -39,6 +43,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+#Function which return the value of the inversed Matrix. 
+#The function just return the inversed matrix if the inversed Matrix has been already calculated 
+#otherwise it perform the operation using the tool solve()
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
